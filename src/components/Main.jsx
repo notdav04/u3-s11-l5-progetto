0215c4affab025e6f6c315f6f3d22c494f5cf434
notdav04 <davide.alonzi26@gmail.com>
2024-12-13 17:20:44 +0100

    minor fixes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Col, Row, Card } from "react-bootstrap";
+import { Col, Row, Card, Container } from "react-bootstrap";
 import { useDispatch } from "react-redux";
 import { changeSong } from "../redux/actions";
 
@@ -41,123 +41,125 @@ const Main = () => {
   }, []);
   return (
     <Col xs={12} md={9} className="offset-md-3 mainpage">
-      <Row>
-        <Col xs={9} lg={11} className="mainLinks d-none d-md-flex">
-          <a href="#">TRENDING</a>
-          <a href="#">PODCAST</a>
-          <a href="#">MOODS AND GENRES</a>
-          <a href="#">NEW RELEASES</a>
-          <a href="#">DISCOVER</a>
-        </Col>
-      </Row>
-      <Row>
-        <Col xs={10}>
-          <h2 className="text-light mt-5">Rock Classics</h2>
-          <Row xs={1} sm={2} lg={3} xl={4} className="imgLinks py-3">
-            {rock.length > 0 &&
-              rock.map((element, index) => {
-                return (
-                  <Col key={index}>
-                    <Card
-                      text="light
-                "
-                      onClick={() => {
-                        dispatch(changeSong(element));
-                      }}
-                      style={{
-                        width: "18rem",
-                        height: "400px",
-                        backgroundColor: "transparent",
-                        border: "none"
-                      }}
-                    >
-                      <Card.Img
-                        variant="top"
-                        src={element.album.cover_medium}
-                      />
-                      <Card.Body>
-                        <Card.Title>{element.title}</Card.Title>
-                        <Card.Text>{element.artist.name}</Card.Text>
-                      </Card.Body>
-                    </Card>
-                  </Col>
-                );
-              })}
-          </Row>
-        </Col>
-      </Row>
-      <Row>
-        <Col xs={10}>
-          <h2 className="text-light">Pop Classics</h2>
-          <Row xs={1} sm={2} lg={3} xl={4} className="imgLinks py-3">
-            {pop.length > 0 &&
-              pop.map((element, index) => {
-                return (
-                  <Col key={index}>
-                    <Card
-                      text="light
-                "
-                      onClick={() => {
-                        dispatch(changeSong(element));
-                      }}
-                      style={{
-                        width: "18rem",
-                        height: "400px",
-                        backgroundColor: "transparent",
-                        border: "none"
-                      }}
-                    >
-                      <Card.Img
-                        variant="top"
-                        src={element.album.cover_medium}
-                      />
-                      <Card.Body>
-                        <Card.Title>{element.title}</Card.Title>
-                        <Card.Text>{element.artist.name}</Card.Text>
-                      </Card.Body>
-                    </Card>
-                  </Col>
-                );
-              })}
-          </Row>
-        </Col>
-      </Row>
-      <Row>
-        <Col xs={10}>
-          <h2 className="text-light">HipHop Classics</h2>
-          <Row xs={1} sm={2} lg={3} xl={4} className="imgLinks py-3">
-            {hiphop.length > 0 &&
-              hiphop.map((element, index) => {
-                return (
-                  <Col key={index}>
-                    <Card
-                      text="light
-                "
-                      onClick={() => {
-                        dispatch(changeSong(element));
-                      }}
-                      style={{
-                        width: "18rem",
-                        height: "400px",
-                        backgroundColor: "transparent",
-                        border: "none"
-                      }}
-                    >
-                      <Card.Img
-                        variant="top"
-                        src={element.album.cover_medium}
-                      />
-                      <Card.Body>
-                        <Card.Title>{element.title}</Card.Title>
-                        <Card.Text>{element.artist.name}</Card.Text>
-                      </Card.Body>
-                    </Card>
-                  </Col>
-                );
-              })}
-          </Row>
-        </Col>
-      </Row>
+      <Container fluid>
+        <Row>
+          <Col xs={9} lg={11} className="mainLinks d-none d-md-flex">
+            <a href="#">TRENDING</a>
+            <a href="#">PODCAST</a>
+            <a href="#">MOODS AND GENRES</a>
+            <a href="#">NEW RELEASES</a>
+            <a href="#">DISCOVER</a>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={10}>
+            <h2 className="text-light mt-5">Rock Classics</h2>
+            <Row xs={1} sm={2} lg={3} xl={4} className="imgLinks py-3">
+              {rock.length > 0 &&
+                rock.map((element, index) => {
+                  return (
+                    <Col key={index}>
+                      <Card
+                        text="light
+                    "
+                        onClick={() => {
+                          dispatch(changeSong(element));
+                        }}
+                        style={{
+                          width: "18rem",
+                          height: "400px",
+                          backgroundColor: "transparent",
+                          border: "none"
+                        }}
+                      >
+                        <Card.Img
+                          variant="top"
+                          src={element.album.cover_medium}
+                        />
+                        <Card.Body>
+                          <Card.Title>{element.title}</Card.Title>
+                          <Card.Text>{element.artist.name}</Card.Text>
+                        </Card.Body>
+                      </Card>
+                    </Col>
+                  );
+                })}
+            </Row>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={10}>
+            <h2 className="text-light">Pop Classics</h2>
+            <Row xs={1} sm={2} lg={3} xl={4} className="imgLinks py-3">
+              {pop.length > 0 &&
+                pop.map((element, index) => {
+                  return (
+                    <Col key={index}>
+                      <Card
+                        text="light
+                    "
+                        onClick={() => {
+                          dispatch(changeSong(element));
+                        }}
+                        style={{
+                          width: "18rem",
+                          height: "400px",
+                          backgroundColor: "transparent",
+                          border: "none"
+                        }}
+                      >
+                        <Card.Img
+                          variant="top"
+                          src={element.album.cover_medium}
+                        />
+                        <Card.Body>
+                          <Card.Title>{element.title}</Card.Title>
+                          <Card.Text>{element.artist.name}</Card.Text>
+                        </Card.Body>
+                      </Card>
+                    </Col>
+                  );
+                })}
+            </Row>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={10}>
+            <h2 className="text-light">HipHop Classics</h2>
+            <Row xs={1} sm={2} lg={3} xl={4} className="imgLinks py-3">
+              {hiphop.length > 0 &&
+                hiphop.map((element, index) => {
+                  return (
+                    <Col key={index}>
+                      <Card
+                        text="light
+                    "
+                        onClick={() => {
+                          dispatch(changeSong(element));
+                        }}
+                        style={{
+                          width: "18rem",
+                          height: "400px",
+                          backgroundColor: "transparent",
+                          border: "none"
+                        }}
+                      >
+                        <Card.Img
+                          variant="top"
+                          src={element.album.cover_medium}
+                        />
+                        <Card.Body>
+                          <Card.Title>{element.title}</Card.Title>
+                          <Card.Text>{element.artist.name}</Card.Text>
+                        </Card.Body>
+                      </Card>
+                    </Col>
+                  );
+                })}
+            </Row>
+          </Col>
+        </Row>
+      </Container>
     </Col>
   );
 };
--- src/components/Playerbar.jsx
+++ src/components/Playerbar.jsx
@@ -16,67 +16,73 @@ const Playerbar = () => {
   const dispatch = useDispatch();
   useEffect(() => {}, [playerbarSong]);
   return (
-    <Container fluid className="fixed-bottom bg-container pt-1">
-      <Row className="h-100">
-        {playerbarSong && (
-          <Col lg={2} className="offset-lg-2 d-flex align-items-center">
-            <img src={playerbarSong.album.cover_small} alt="" />
-            <div style={{ width: "200px" }} className="text-light ms-2">
-              <p className="mb-0">{playerbarSong.title}</p>
-              <p className=" mb-0">{playerbarSong.artist.name}</p>
-            </div>
-            {favourites.includes(playerbarSong.title) ? (
-              <HeartFill
-                onClick={() => {
-                  dispatch(removefavAction(playerbarSong.title));
-                }}
-                className="text-success fs-3 pe-1"
-              />
-            ) : (
-              <Heart
-                onClick={() => {
-                  dispatch(addfavAction(playerbarSong.title));
-                }}
-                className="text-light fs-3 pe-1"
-              />
-            )}
-          </Col>
-        )}
-        <Col
-          lg={playerbarSong ? 8 : 10}
-          className={playerbarSong ? "" : "offset-lg-2"}
-        >
-          <Row
-            className={`h-100 flex-column  justify-content-center ${
-              !playerbarSong ? " align-items-center " : "offset-1"
-            }`}
-            style={{ height: "100%" }}
-          >
-            <Col xs={6} md={playerbarSong ? 6 : 4} className="playerControls">
-              <div className="d-flex">
-                <a href="#">
-                  <img src={shuffle} alt="shuffle" />
-                </a>
-                <a href="#">
-                  <img src={prev} alt="prev" />
-                </a>
-                <a href="#">
-                  <img src={play} alt="play" />
-                </a>
-                <a href="#">
-                  <img src={next} alt="next" />
-                </a>
-                <a href="#">
-                  <img src={repeat} alt="repeat" />
-                </a>
+    <Container
+      fluid
+      className="fixed-bottom bg-container m-0 p-0 "
+      style={{ maxWidth: "100vw" }}
+    >
+      <div>
+        <Row className="h-100 m-0">
+          {playerbarSong && (
+            <Col lg={2} className="offset-lg-2 d-flex align-items-center">
+              <img src={playerbarSong.album.cover_small} alt="" />
+              <div style={{ width: "200px" }} className="text-light ms-2">
+                <p className="mb-0">{playerbarSong.title}</p>
+                <p className=" mb-0">{playerbarSong.artist.name}</p>
               </div>
-              <div className="progress mt-3">
-                <div role="progressbar"></div>
-              </div>
+              {favourites.includes(playerbarSong.title) ? (
+                <HeartFill
+                  onClick={() => {
+                    dispatch(removefavAction(playerbarSong.title));
+                  }}
+                  className="text-success fs-3 pe-1"
+                />
+              ) : (
+                <Heart
+                  onClick={() => {
+                    dispatch(addfavAction(playerbarSong.title));
+                  }}
+                  className="text-light fs-3 pe-1"
+                />
+              )}
             </Col>
-          </Row>
-        </Col>
-      </Row>
+          )}
+          <Col
+            lg={playerbarSong ? 8 : 10}
+            className={playerbarSong ? "" : "offset-lg-2"}
+          >
+            <Row
+              className={`h-100 flex-column  justify-content-center ${
+                !playerbarSong ? " align-items-center " : "offset-1"
+              }`}
+              style={{ height: "100%" }}
+            >
+              <Col xs={6} md={playerbarSong ? 6 : 4} className="playerControls">
+                <div className="d-flex">
+                  <a href="#">
+                    <img src={shuffle} alt="shuffle" />
+                  </a>
+                  <a href="#">
+                    <img src={prev} alt="prev" />
+                  </a>
+                  <a href="#">
+                    <img src={play} alt="play" />
+                  </a>
+                  <a href="#">
+                    <img src={next} alt="next" />
+                  </a>
+                  <a href="#">
+                    <img src={repeat} alt="repeat" />
+                  </a>
+                </div>
+                <div className="progress mt-3">
+                  <div role="progressbar"></div>
+                </div>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </div>
     </Container>
   );
 };
